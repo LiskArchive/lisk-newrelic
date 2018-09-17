f6596a0fd735e797894bd1c6dd9f6d6768c94031
Nazar Hussain <nazarhussain@gmail.com>
2018-09-17 09:18:47 +0200

    Add instrumentation for job queue

---
+++ src/instrumentation/job_queue.js
@@ -0,0 +1,46 @@
+/*
+ * LiskHQ/lisk-newrelic
+ * Copyright © 2017 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+
+const debug = require('debug')('newrelic:lisk:job_queue');
+
+module.exports = function initialize(shim, jobQueue, moduleName) {
+	debug('init %s', moduleName);
+
+	const newrelic = this;
+
+	// eslint-disable-next-line no-shadow
+	shim.wrap(jobQueue, ['register'], (shim, originalRegister, fnName) => {
+		debug('wrapping %s', fnName);
+
+		if (shim.isWrapped(originalRegister)) {
+			return originalRegister;
+		}
+
+		return function wrappedRegister(name, originalJob, time) {
+			function wrappedJob(cb) {
+				newrelic.startBackgroundTransaction(name, 'jobQueue', () => {
+					const transaction = newrelic.getTransaction();
+					originalJob.call(this, () => {
+						transaction.end();
+						cb.call(this);
+					});
+				});
+			}
+
+			originalRegister.call(this, name, wrappedJob, time);
+		};
+	});
+};
