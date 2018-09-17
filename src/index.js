34468d2a366f9c8de05d023e484a8314fc016b71
Nazar Hussain <nazarhussain@gmail.com>
2018-09-17 09:19:22 +0200

    Expose interface of all instrumentation

--- src/index.js
+++ src/index.js
@@ -14,4 +14,129 @@
  *
  */
 
-export default 'Hello world';
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+const jobQueueInstrument = require('./instrumentation/job_queue');
+const pgPromiseInstrument = require('./instrumentation/pg_promise');
+const swaggerNodeRunnerInstrument = require('./instrumentation/swagger_node_runner');
+const modulesWithCallbackInstrument = require('./instrumentation/modules_with_callback');
+
+const defaults = {
+	exitOnFailure: true,
+	rootPath: null,
+};
+
+class LiskNewRelic {
+	/**
+	 *
+	 * @param {Object} newrelic - newrelic object
+	 * @param {Object} options - Options object
+	 * @param {boolean} options.exitOnFailure - The logger object used by this plugin
+	 * @param {string} options.rootPath - Root path to the lisk
+	 */
+	constructor(newrelic, options = {}) {
+		this.newrelic = newrelic;
+		this.config = Object.assign({}, defaults, options);
+
+		assert(options.rootPath, 'root path is required parameter');
+
+		this.errorHandler = function instrumentationError(error) {
+			// this refers to newrelic and it have its own logger
+			this.newrelic.shim.logger.fatal(error);
+
+			if (this.config.exitOnFailure) {
+				process.exit(1);
+			}
+		}.bind(this);
+	}
+
+	instrumentDatabase() {
+		this.newrelic.instrumentDatastore({
+			moduleName: 'pg-promise',
+			onRequire: pgPromiseInstrument,
+			onError: this.errorHandler,
+		});
+	}
+
+	instrumentWeb() {
+		this.newrelic.instrumentWebframework({
+			moduleName: 'swagger-node-runner',
+			onRequire: swaggerNodeRunnerInstrument,
+			onError: this.errorHandler,
+		});
+
+		const controllerFolder = '/api/controllers/';
+		const controllerMethodExtractor = (shim, controller) =>
+			Object.getOwnPropertyNames(controller).filter(name =>
+				shim.isFunction(controller[name]),
+			);
+
+		fs.readdirSync(this.config.rootPath + controllerFolder).forEach(file => {
+			if (path.basename(file) !== 'index.js') {
+				const controllerPath = `${this.config.rootPath}${controllerFolder}${
+					file
+				}`;
+				const identifier = `api.controllers.${path
+					.basename(file)
+					.split('.')
+					.slice(0, -1)
+					.join('.')}`;
+
+				this.instrumentCallbackMethods(
+					controllerPath,
+					identifier,
+					controllerMethodExtractor,
+				);
+			}
+		});
+	}
+
+	instrumentBackgroundJobs() {
+		this.newrelic.instrument({
+			moduleName: '../helpers/jobs_queue.js',
+			onRequire: jobQueueInstrument.bind(this.newrelic),
+			onError: this.errorHandler,
+		});
+	}
+
+	/**
+	 * Helper to instrument some modules with callback methods
+	 *
+	 * @param {string} modulePath - Module path which will be required
+	 * @param {string} moduleIdentifier - Module path which will be required
+	 * @param {Array.<string> | Function} callbackFunctions - Array of callback methods or function to
+	 * extract from module
+	 */
+	instrumentCallbackMethods(modulePath, moduleIdentifier, callbackFunctions) {
+		assert(modulePath, 'Must specify module path.');
+		assert(moduleIdentifier, 'Must specify module identifier.');
+		assert(
+			this.newrelic.shim.isArray(callbackFunctions) ||
+				this.newrelic.shim.isFunction(callbackFunctions),
+			'Must specify callbackFunctions as array of strings or as a function',
+		);
+
+		this.newrelic.instrument({
+			moduleName: modulePath,
+			onRequire: modulesWithCallbackInstrument.bind(this,
+				moduleIdentifier,
+				callbackFunctions,
+			),
+			onError: this.errorHandler,
+		});
+	}
+}
+
+/**
+ *
+ * @param {Object} newrelic - newrelic object
+ * @param {Object} options - Options object
+ * @param {boolean} options.exitOnFailure - The logger object used by this plugin
+ * @param {string} options.rootPath - Root path to the lisk
+ *
+ * @return {LiskNewRelic}
+ */
+module.exports = function newrelicLiskGenerator(newrelic, options) {
+	return new LiskNewRelic(newrelic, options);
+};
